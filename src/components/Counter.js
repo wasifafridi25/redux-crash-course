222083a6fd0c72ac2adcab3b941a80eac03f53ff
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-12-31 17:31:48 -0800

    Added Slice

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 export default function Counter() {
-    const [counter, setCounter] = useState(0)
+  // const count = useSelector(state => {
+  //   console.log(state)
+  //   return state.counter.count
 
-    const increment = () => {
-        setCounter(counter + 1)
-    }
-    const decrement = () => {
-        setCounter(prevValue => prevValue - 1)
-    }
+  // })
+
+  // one liner
+  const count = useSelector(state => state.counter.count) // useSelector to use the state
+  
+  //To use the actions, those functions(incremenet, decrement, etc) we use the useDispatch function
+  
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h1>The count is: {counter}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <h1>The count is: {count}</h1>
+      <button>Increment</button>
+      <button>Decrement</button>
     </div>
   )
 }
+
+
+
+
+
+
+
+    // const [count, setCount] = useState(0)
+
+    // const increment = () => {
+    //     setCount(count + 1)
+    // }
+    // const decrement = () => {
+    //     setCount(prevValue => prevValue - 1)
+    // }
